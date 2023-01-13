77d99d7c587419aceb7a7e3c28f45b4feeadfc29
Peter <peter_s_96@live.com>
2023-01-12 21:52:42 -0800

    fixed folder structure for better readability

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const apiRouter = express.Router();
 
-const usersRouter = require("./api/users");
+const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
 
-const postsRouter = require("./api/posts");
+const postsRouter = require("./posts");
 apiRouter.use("/posts", postsRouter);
-
-apiRouter.use((error, req, res, next) => {
-  res.send(error);
-});
 
 module.exports = apiRouter;
--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const PORT = 3000;
 const express = require("express");
 const server = express();
 
-const apiRouter = express.Router();
-
 const morgan = require("morgan");
 server.use(morgan("dev"));
 
 server.use(express.json()); // read incoming JSON requests. req headers has to be Content-Type: application/json
-
-// const apiRouter = require("./api");
-server.use("/api", apiRouter);
 
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
@@ -23,11 +18,14 @@ server.use((req, res, next) => {
 const { client } = require("./db");
 client.connect();
 
-const usersRouter = require("./api/users");
-apiRouter.use("/users", usersRouter);
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
 
-const postsRouter = require("./api/posts");
-apiRouter.use("/posts", postsRouter);
+// const usersRouter = require("./api/users");
+// apiRouter.use("/users", usersRouter);
+
+// const postsRouter = require("./api/posts");
+// apiRouter.use("/posts", postsRouter);
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
